273b38a9b59573b9affee98d13dbeacb603d6015
baivong <baivong@users.noreply.github.com>
2015-05-29 06:24:24 +0700

    v2.0.1

---
+++ cdn/main.js
@@ -0,0 +1,51 @@
+fmbackup.translation({
+    langcode: "en",
+    ex: {
+        title: "Export Template",
+        tooltip: "In this table, you can also export [[changed template]] to a *.zip file and save it to your computer.\nTo start, click {{Check}} button to get version information and the list of templates that you want to export. Then click {{Ok}}, wait for few seconds and you are good to go.",
+        checkall: "Pick all",
+        unpublish: "Waiting Templates",
+        firsttip: "Pick categories you need then click {{Check}}.",
+        download: "Downloading your templates. Click here if its take too long!",
+        download2: "Your templates have been downloaded.",
+        notemplate: "There's no template archive needed",
+        sumtemplate: "The number of template archive needed it's",
+        presssubmit: "Click {{Ok}} to start!",
+        pressrefresh: "Click {{Start over}} to change your options!",
+        loading: "Creating Zip file..."
+    },
+    im: {
+        title: "Import Template",
+        tooltip: "In this table, you can also import template from a *.zip file. Attention: your version must match with the version of the template you are going to import.\nTo start, click [[Open file]] (Browse...) pick your template zipped file (*.zip). Then click {{Ok}}, wait for few seconds and you are good to go.",
+        choose: "Open zipped file",
+        notpublish: "Not publish Template",
+        firsttip: "Pick your zipped file then click {{Ok}}.",
+        notname: "Opps! File name is <<not valid>>.",
+        notversion: "Opps! This template require ",
+        source: "Source",
+        version: "Version",
+        time: "Last update",
+        count: "Template(s)",
+        updateAll: "All done!",
+        loading: "Updating template..."
+    },
+    option: "Options",
+    simpleclick: "One-Click mode",
+    wail: "Please hold on a second...",
+    notsupport: "Your browser does not support this application!",
+    requestlimit: "Access denied! Progress will start over in a while",
+    reloadtemplate: "Reload template ",
+    errortemplate: "Bad template ",
+    second: "s.",
+    filtering: "Template filtering...",
+    checkone: "Pick at least 1 item",
+    progressrun: "Progress is running.",
+    bt: {
+        filter: "Check",
+        refresh: "Start over",
+        submit: "Ok",
+        start: "START",
+    }
+});
+fmbackup.version("2.0.1");
+fmbackup.init();
